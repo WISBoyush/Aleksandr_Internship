6ac00d0db0cf005b03c12f3c6417404b76371358
WISBoyush <a.boyushenko@wis.software>
2022-06-24 14:10:33 +0300

    [feature] [4_1] add function that convert string type to int

---
+++ py_fundamentals/String_to_int.py
@@ -0,0 +1,27 @@
+# Напишите функцию, которая будет преобразовывать строку в число аналогично функции int только без ее использования.
+# Функция должна иметь набор проверок и поднимать исключения если переданные данные не являются валидными.
+
+
+def string_to_int(string):
+    if not string.startswith('-') and not string.isdigit():
+        raise ValueError(f'Вами введено не число \n\n Ошибка в ({string})')
+    elif string.startswith('-') and not string[1:].isdigit():
+        raise ValueError(f'Вами введено не число \n\n Ошибка в ({string})')
+    else:
+        positive = False if string.startswith('-') else True
+        string = string[1:] if not positive else string
+        number = 0
+        for i in string:
+            number *= 10
+            number += (ord(i) - 48)
+        return number if positive else number * -1
+
+
+try:
+    print(string_to_int('1340324'))
+
+    print(string_to_int('-123'))
+
+    print(string_to_int('0asd123'))
+except ValueError as e:
+    print(f'__!!! WARNING !!!__\n{e}')
